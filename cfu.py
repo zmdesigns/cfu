b7336cada8cb2c37ef127dec7effbfd909d09e54
zackmdesigns <zackmdesigns@gmail.com>
2021-11-19 15:08:56 -0800

    Improvements to 'full' storage: Use full path when reading and writing files. Normalized way of getting filename of file. Moved to retrieving text content from divs in a set and storing content as a div per line. This is to make seeing what has changed easier. Switched to requests lib for requesting the webpage to increase the amount of websites the script works with. Added sending of a modern chrome user-agent header in request to further increase amount of webpages the script works with. Added some console feedback as script is executing to help with debugging. Added console output when checking a website set to 'full', which will display the changed text in a new div->old div format. Updated check function to use 'sectionName' instead of 'name' to normalize the use of that variable accross script.

--- cfu.py
+++ cfu.py
@@ -6,18 +6,29 @@ checkForUpdates.py
 from bs4 import BeautifulSoup as bs
 import hashlib
 from urllib.request import (urlopen)
+import requests
 import telebot
 import os
 import click
 import configparser
 import difflib
 
-def getTextContent(url):
-    soup = bs(urlopen(url),features="lxml")
-    textContent = ''
+def getWebContent(url):
+    click.echo('Requesting ' + url + '...')
+    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'}
+    r = requests.get(url, headers=headers)
+    click.echo('Received ' + url + '! Status Code: ' + str(r.status_code))
+
+    soup = bs(r.text,features="lxml")
 
     for not_wanted_tag in soup.find_all(['script','style','Cdata']):
         not_wanted_tag.decompose()
+
+    return soup
+
+def getTextContent(url):
+    soup = getWebContent(url)
+    textContent = ''
 
     for div_tag in soup.find_all('div'):
         textContent += div_tag.getText()
@@ -25,17 +36,53 @@ def getTextContent(url):
     textContent = " ".join(textContent.split())
     return textContent
 
+def getSetContent(url):
+    soup = getWebContent(url)
+    setContent = set()
+
+    for div_tag in soup.find_all('div'):
+        divText = div_tag.getText().replace('\n','')
+        divText = " ".join(divText.split())
+        if divText != '':
+            setContent.add(divText + '\n')
+
+    return setContent
+
 def getHash(url):
     textContent = getTextContent(url)
     hashValue = hashlib.md5(textContent.encode("utf-8")).hexdigest()
     return hashValue
+
+def findSetDiff(a, b):
+    aDiff = []
+    bDiff = []
+    for aV in a:
+        if aV not in b:
+            aDiff.append(aV)
     
+    for bV in b:
+        if bV not in a:
+            bDiff.append(bV)
+
+    changes = []
+    for adI, adV in enumerate(aDiff):
+        if adI < len(bDiff):
+            changes.append(adV + '->' + bDiff[adI])
+        else:
+            changes.append('added ' + adV)
+    if len(bDiff) > len(aDiff):
+        i = len(aDiff) - 1
+        while i < len(bDiff):
+            changes.append('removed ' + bDiff[i])
+
+
+    return ','.join(changes)
 
 class Cfu(object):
     def __init__(self):
         self.config = configparser.ConfigParser()
-        scriptDir = os.path.dirname(__file__)
-        self.configFilePath = os.path.join(scriptDir, 'configCfu.ini')
+        self.scriptDir = os.path.dirname(__file__)
+        self.configFilePath = os.path.join(self.scriptDir, 'configCfu.ini')
 
         if not os.path.exists(self.configFilePath):
             self.setup('','','')
@@ -54,11 +101,11 @@ class Cfu(object):
     def update(self, sectionName, newValue):
         """ Update the value in config and send a message """
         self.sendMessage(sectionName)
-
+        click.echo('updating ' + sectionName + ' length: ' + str(len(newValue)))
         if self.config[sectionName]['value_type'] == 'full':
-            filename = sectionName + '.txt'
-            with open(filename, 'w') as f:
-                f.write(newValue)
+            filePath = os.path.join(self.scriptDir, self.config[sectionName]['value'])
+            with open(filePath, 'w') as f:
+                f.writelines(newValue)
         else:
             self.config[sectionName]['value'] = newValue
             self.writeConfig()
@@ -87,23 +134,28 @@ class Cfu(object):
         self.config[name] = {'url': url, 'value': value, 'value_type': vtype}
         self.writeConfig()
 
-    def check(self, name):
+    def check(self, sectionName):
         """ Compare value stored from last check to live, if it's different, it's updated """
-        url = self.config[name]['url']
+        url = self.config[sectionName]['url']
         value = ''
         newValue = ''
-        valueType = self.config[name]['value_type']
+        valueType = self.config[sectionName]['value_type']
         
         if valueType == 'hash':
-            value = self.config[name]['value']
+            value = self.config[sectionName]['value']
             newValue = getHash(url)
         elif valueType == 'full':
-            with open(self.config[name]['value'], 'r') as file:
-                value = file.read()
-            newValue = getTextContent(url)
+            filePath = os.path.join(self.scriptDir, self.config[sectionName]['value'])
+            click.echo('reading file: ' + filePath)
+            with open(filePath, 'r') as file:
+                value = set(file.readlines())
+            newValue = getSetContent(url)
+
+            diff = findSetDiff(newValue, value)
+            click.echo(diff)
 
         if newValue != value:
-            self.update(name, newValue)
+            self.update(sectionName, newValue)
         
 
 @click.group()
